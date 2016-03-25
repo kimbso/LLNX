eb0a4f1cf0f789733ed278c30a34d42e897a6cd6
kimbso <kimbso@bc.edu>
2016-03-25 13:01:54 -0400

    Initial Commit

---
+++ app/src/main/java/com/minkim/llnx/ManageLlnkActivity.java
@@ -0,0 +1,13 @@
+package com.minkim.llnx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManageLlnkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_llnk);
+    }
+}
---
+++ app/src/main/java/com/minkim/llnx/SendLlnkActivity.java
@@ -0,0 +1,13 @@
+package com.minkim.llnx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendLlnkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_llnk);
+    }
+}
